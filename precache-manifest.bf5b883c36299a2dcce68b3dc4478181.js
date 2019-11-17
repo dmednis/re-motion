492aa3cdf1ddbdefeb6887dac72bef872a56f338
Dāvis Mednis <davi.mednis@prezi.com>
2019-11-17 09:26:57 +0200

    Updates

---
+++ precache-manifest.bf5b883c36299a2dcce68b3dc4478181.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdc9c71be6588be948505217aa47ce5e",
+    "url": "/re-motion/index.html"
+  },
+  {
+    "revision": "ca9ce7209f93961a8270",
+    "url": "/re-motion/static/css/2.b96c6e41.chunk.css"
+  },
+  {
+    "revision": "758dc0ef970256851d43",
+    "url": "/re-motion/static/css/main.64c352c8.chunk.css"
+  },
+  {
+    "revision": "ca9ce7209f93961a8270",
+    "url": "/re-motion/static/js/2.6bdc5718.chunk.js"
+  },
+  {
+    "revision": "758dc0ef970256851d43",
+    "url": "/re-motion/static/js/main.a675862a.chunk.js"
+  },
+  {
+    "revision": "74fcf65a0658fab8a30a",
+    "url": "/re-motion/static/js/runtime-main.2b636319.js"
+  },
+  {
+    "revision": "a9de0ae961254cfc657e4b865afdc41c",
+    "url": "/re-motion/static/media/remotion-small.a9de0ae9.png"
+  }
+]);
